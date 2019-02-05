eb6e0c89982a24e19e2fa543b997c173693b8217
Angela Uwase <angelakarenzi5@gmail.com>
2019-02-05 09:21:11 +0200

    adding imports on component.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {  HttpClientModule }  from '@angular/common/http'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { GitComponent } from './profile/profile.component';
@@ -14,7 +14,8 @@ import { ProfileFormComponent } from './profile-form/profile-form.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,6 +1,7 @@
 import { GitService } from "../gits/git.service";
 import { Component, OnInit } from "@angular/core";
 import { User } from "../user";
+import { HttpClient } from '@angular/common/http'
 
 @Component({
   selector: "app-profile",
