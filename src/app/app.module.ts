07fb7bd3b7f022cfb05fb68727dedfbb7060601e
Angela Uwase <angelakarenzi5@gmail.com>
2019-02-05 18:07:16 +0200

    editing profile component.html

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,25 @@
-import { BrowserModule } from "@angular/platform-browser";
-import { NgModule } from "@angular/core";
-import { HttpClientModule } from "@angular/common/http";
-import { GitService } from "./gits/git.service";
-import { FormComponent } from "./form/form.component";
-import { AppComponent } from "./app.component";
-import { ProfileComponent } from "./profile/profile.component";
-// import { FormComponent } from "./form/form";
-import { FormsModule }   from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { GitService } from './gits/git.service';
 
+import {FormsModule} from '@angular/forms';
+import { AppComponent } from './app.component';
+import { ProfileComponent } from './profile/profile.component';
+import { FormComponent } from './form/form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProfileComponent,
-    FormComponent
+    FormComponent,
   ],
-  imports: [BrowserModule, HttpClientModule, FormsModule],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    FormsModule
+  ],
   providers: [GitService],
   bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }
