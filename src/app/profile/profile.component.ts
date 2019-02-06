67f4bb14d3706729174d83584b541813d36ada80
Angela Uwase <angelakarenzi5@gmail.com>
2019-02-06 08:58:48 +0200

    re-styling

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,24 +1,22 @@
-import { Component, OnInit } from "@angular/core";
-
-import { User } from "../user";
-import { GitService } from "../gits/git.service";
+import { Component, OnInit } from '@angular/core';
+import { GitService } from '../gits/git.service';
+import {User} from '../user';
 
 @Component({
-  selector: "app-profile",
-  templateUrl: "./profile.component.html",
-  styleUrls: ["./profile.component.css"],
-  providers: [GitService]
+  selector: 'app-profile',
+  templateUrl: './profile.component.html',
+  styleUrls: ['./profile.component.css'],
+  providers:[GitService]
 })
 export class ProfileComponent implements OnInit {
   user: User;
-
-  constructor(
-    private profileService: GitService,
-    public repoService: GitService
-  ) {
-    this.user = this.profileService.user;
-  }
+  
+ constructor(private profileService: GitService, public repoService: GitService) { 
+ this.user = this.profileService.user;
+ }
   ngOnInit() {
     this.user = this.profileService.user;
+
   }
+
 }
