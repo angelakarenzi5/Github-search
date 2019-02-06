03d3812211c7fdcfa898204e08560bbae436e2fc
Angela Uwase <angelakarenzi5@gmail.com>
2019-02-06 08:46:04 +0200

    Fixing form component.ts

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,26 +1,18 @@
-import { Component, OnInit } from "@angular/core";
-import { GitService } from "../gits/git.service";
+import { Component, OnInit } from '@angular/core';
+import { GitService } from '../gits/git.service';
 
 @Component({
-  selector: "app-form",
-  templateUrl: "./form.component.html",
-  styleUrls: ["./form.component.css"]
+  selector: 'app-profile-form',
+  templateUrl: './form.component.html',
+  styleUrls: ['./form.component.css']
 })
 export class FormComponent implements OnInit {
-    user={
-        user: ""
-    }
 
-    onKey(event:any) {
-        this.user += event.target.value;
-    }
-      submitSearch(name) {
-   this.gitService.getProfileInfo(name.target.value);
-   this.repositoryService.getRepoInfo(name.target.value);
+  submitSearch(name) {
+    this.profileService.getProfileInfo(name.target.value);
+    this.repoService.getRepoInfo(name.target.value);
   }
-  constructor(
-    private gitService: GitService,
-    private repositoryService: GitService
-  ) {}
-  ngOnInit() {}
-}
+   constructor(private profileService: GitService, private repoService: GitService) { }
+  ngOnInit() {
+  }
+ }
